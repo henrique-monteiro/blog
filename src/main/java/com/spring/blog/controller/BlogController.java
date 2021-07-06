23e8304fc1942c482488640b7e89c840185e9d9e
henrique-monteiro <henrique-monteiro@hotmail.com>
2021-07-06 08:06:57 -0300

    feat: find post details by ID

--- src/main/java/com/spring/blog/controller/BlogController.java
+++ src/main/java/com/spring/blog/controller/BlogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,8 +21,16 @@ public class BlogController {
 	@RequestMapping(value="/posts", method=RequestMethod.GET)
     public ModelAndView getPosts(){
         ModelAndView mv = new ModelAndView("posts");
-        List<Post> posts = blogService.findAll();
+        List<Post> posts = blogService.findAll();	//retorna a lista de posts
         mv.addObject("posts", posts);
+        return mv;
+    }
+	
+	@RequestMapping(value="/posts/{id}", method=RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){    //recebe o parâmetro (PathVariable("id"), tipo do id)
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = blogService.findById(id);	//retorna um post específico
+        mv.addObject("post", post);
         return mv;
     }
 
