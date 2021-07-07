de0b66d0a691575dbd599a76aa8f99d1cd071e14
henrique-monteiro <henrique-monteiro@hotmail.com>
2021-07-07 07:31:56 -0300

    feat: add method savePost and postForm.html to insert data in database

--- src/main/java/com/spring/blog/controller/BlogController.java
+++ src/main/java/com/spring/blog/controller/BlogController.java
@@ -1,13 +1,16 @@
 package com.spring.blog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.blog.model.Post;
 import com.spring.blog.service.BlogService;
@@ -20,7 +23,7 @@ public class BlogController {
 	
 	@RequestMapping(value="/posts", method=RequestMethod.GET)
     public ModelAndView getPosts(){
-        ModelAndView mv = new ModelAndView("posts");
+        ModelAndView mv = new ModelAndView("posts");	//"posts é o nome do arquivo em html"
         List<Post> posts = blogService.findAll();	//retorna a lista de posts
         mv.addObject("posts", posts);
         return mv;
@@ -28,10 +31,27 @@ public class BlogController {
 	
 	@RequestMapping(value="/posts/{id}", method=RequestMethod.GET)
     public ModelAndView getPostDetails(@PathVariable("id") long id){    //recebe o parâmetro (PathVariable("id"), tipo do id)
-        ModelAndView mv = new ModelAndView("postDetails");
+        ModelAndView mv = new ModelAndView("postDetails");	//"postDetails é o nome do arquivo em html"
         Post post = blogService.findById(id);	//retorna um post específico
-        mv.addObject("post", post);
+        mv.addObject("post", post);	//"post" é o nome da variavel no arquivo html
         return mv;
     }
+	
+	@RequestMapping(value="/newpost", method=RequestMethod.GET)
+    public String getPostForm(){	//metodo apenas chama o form para incluir um novo post
+        return "postForm";
+    }
+	
+	@RequestMapping(value="/newpost", method=RequestMethod.POST)
+    public String savePost(Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        blogService.save(post);
+        return "redirect:/posts";
+    }
+	
 
 }
